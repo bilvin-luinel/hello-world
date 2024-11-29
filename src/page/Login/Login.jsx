4b9dbe80f441b5740bc8acd03d71615dc34783d0
DominicKang <On MacBookPro>
2024-11-29 17:55:27 +0900

    20241129-PM0555

--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -4,25 +4,39 @@ import ss from './Login.module.css'
 const Login = () => {
 
 
-    // const array = [{ name: 'john', age: 25, job: 'programmer' }, { name: '유제', age: 23, job: 'ceo' }, { name: 'maria', age: 34, job: 'woman' }, { name: 'jane', age: 40, job: '주부' }]
-    const array2 = [1, 2, 3, 4, 'apple', 999, 'banana']
+    const array = [{ name: 'john', age: 25, job: 'programmer' }, { name: '유제', age: 23, job: 'ceo' }, { name: 'maria', age: 34, job: 'woman' }, { name: 'jane', age: 40, job: '주부' }]
+    const array2 = ['watermelon', 'apple', 'grape', 'banana']
+    const array3 = [{ name: 'john', age: 20 }, { name: 'amy', age: 21 }, { name: 'dominic', age: 22 }, { name: 'steven', age: 23 }, { name: 'youn', age: 24 }, { name: 'gui', age: 40 }]
+
+    const obj = {
+        name: 'amy',
+        age: 25,
+        hobby: 'game'
+    }
+
+    console.log(obj.age)
+
+
 
 
     return (
         <div className={ss.wrap}>
-            <h1>{array2[0]}</h1>
+            {/* <h1>{array2[0]}</h1>
             <h1>{array2[1]}</h1>
             <h1>{array2[2]}</h1>
             <h1>{array2[3]}</h1>
             <h1>{array2[4]}</h1>
             <h1>{array2[5]}</h1>
-            <h1>{array2[6]}</h1>
+            <h1>{array2[6]}</h1> */}
 
-            {array2.map((item, idx) => (
-                <div>
-                    <h1>{item}</h1>
+            {array3.map((item, idx) => (
+                <div key={idx} className={ss.card}>
+                    <h1>이름 : {item.name}</h1>
+                    <h2>나이 : {item.age}</h2>
                 </div>
             ))}
+
+
 
         </div>
 
--- src/page/Product/Product.jsx
+++ src/page/Product/Product.jsx
@@ -2,10 +2,50 @@ import React from 'react'
 import ss from './Product.module.css'
 
 const Product = () => {
+
+    const array = [
+        {
+            url: 'https://gorgo.kr/web/product/medium/202408/b8f1cd6c7ac2a22df583e30d09659dbf.png',
+            title: '다이아윙스 고반발 국제공인구 M5 골프공(30구)',
+            price1: 98000,
+            price2: 59000,
+        },
+        {
+            url: 'https://gorgo.kr/web/product/medium/202408/f51bec4880fe52e2d6309611166feb26.png',
+            title: '다이아윙스 고반발 국제공인구 M3 골프공(30구)',
+            price1: 98000,
+            price2: 59000,
+        },
+        {
+            url: 'https://gorgo.kr/web/product/medium/202408/e586f2dcedda1b7e524e672fb4a5ebd6.png',
+            title: '다이아윙스 고반발 국제공인구 M5 골프공(50구)',
+            price1: 1580000,
+            price2: 200000,
+        },
+        {
+            url: 'https://gorgo.kr/web/product/medium/202408/9bf994be8ce15885b1a2dbe8f9ad7dc0.png',
+            title: '다이아윙스 고반발 국제공인구 M3 골프공(50구)',
+            price1: 158000,
+            price2: 95000,
+        },
+    ]
+
+    const target = 12323423456
     return (
         <div className={ss.wrap}>
             <div className={ss.lv1}></div>
             <div className={ss.lv2}>
+                {array.map((item, idx) => (
+                    <div className={ss.card}>
+                        <img src={item.url} alt='' />
+                        <h3>{item.title}</h3>
+                        <p>
+                            <span className={ss.description1}>{item.price1.toLocaleString()}원</span>
+                            <span className={ss.description2}>{item.price2.toLocaleString()}원</span>
+                            <span className={ss.description3}>{Math.round(100 - (item.price2 / item.price1 * 100))}%</span>
+                        </p>
+                    </div>
+                ))}
             </div>
         </div>
     )
